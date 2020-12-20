a7b77a5f181f828d87891ebf1b66022ae0c5d153
Ralf Meermeier <ralf.meermeier@gmail.com>
2020-12-19 22:20:45 -0500

    Bla

--- Stellar.py
+++ Stellar.py
@@ -1,41 +1,38 @@
 from tkinter import *
 import time
-import picamera
-import RPi.GPIO as GPIO  
 import numpy as np
 import io
 from datetime import datetime, timedelta
 from PIL import Image
 
-   
+def SetupCamera():
+    import picamera
+    camera = picamera.PiCamera()
+    camera.resolution = camera.MAX_RESOLUTION
+    camera_preview_fullscreen = False
+    preview = camera.start_preview()
+
+def PicButtonClick():
+    filename = datetime.now().strftime("%Y%m%d-%H%M%S.jpg")
+    camera.capture(filename)
+
 root = Tk()
 root.title("Pi Camera")
 root.overrideredirect(True)
 root.geometry('%dx%d+0+0' % (root.winfo_screenwidth(), root.winfo_screenheight()))
+root.configure(background='black')
+root.wm_attributes('-alpha', 0.7)  
 
-def touchEvent(event):
-#    print(str(event.x)+" "+str(event.y))
-    pass
+root.rowconfigure(0, weight=1)
+root.rowconfigure(1, weight=1)
+root.rowconfigure(2, weight=1)
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
+root.columnconfigure(2, weight=1)
 
-canvas = Canvas(root, width = root.winfo_screenwidth(), height = root.winfo_screenheight(), bg='black', highlightthickness=0)
-canvas.bind("<Button-1>", touchEvent)
-canvas.pack()
-
-camera = picamera.PiCamera()
-camera.resolution = camera.MAX_RESOLUTION
-camera_preview_fullscreen = False
-preview = camera.start_preview()
-#preview.window = (-50,0, x, y)
- 
-#img = Image.open('ShowOverlay.png')
-#pad = Image.new('RGB', (
-#     ((img.size[0] + 31) // 32) * 32,
-#     ((img.size[1] + 15) // 16) * 16,
-#     ))
-#pad.paste(img, (0, 0))
-#o = camera.add_overlay(pad.tobytes(), format='rgb')
-#o.alpha = 128
-#o.layer = 3
+shutterPic=PhotoImage(file="Shutter.png")
+picButton = Button(root, command=PicButtonClick, image=shutterPic)
+picButton.grid(row=1, column=3)
 
 root.mainloop()
 
